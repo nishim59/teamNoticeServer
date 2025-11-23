f3ca6382b932e0bf1dbc4d265564c6ffb960193a
m-mika-99 <sysevo.mikami@gmail.com>
2025-11-23 17:01:11 +0900

    init Spring Intializrでプロジェクト作成 Spring webだけ依存関係に追加済み

---
+++ src/main/java/com/example/teamNotice/TeamNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamNotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamNoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamNoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamNotice/TeamNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamNotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamNoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
